8fdd84d1ad600c62521c018c66cdc8d5a45ddaca
Sithum Bopitiya <sithumsandeepap@gmail.com>
2022-05-07 16:07:05 +0530

    Fix user cors

--- BFF/src/main/java/com/github/paf/CORSResponseFilter.java
+++ BFF/src/main/java/com/github/paf/CORSResponseFilter.java
@@ -15,9 +15,9 @@ public class CORSResponseFilter implements ContainerResponseFilter {
         MultivaluedMap headers = responseContext.getHeaders();
 
         headers.add("Access-Control-Allow-Origin", "*");
-        //headers.add("Access-Control-Allow-Origin", "http://podcastpedia.org"); //allows CORS requests only coming from podcastpedia.org
         headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
+
     }
 
 }
